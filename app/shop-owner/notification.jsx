f478fc68382887195f7556ecd19c2f9a684544af
Harry <phuothuynh2002@gmail.com>
2024-06-07 09:40:03 +0700

    Create notification page

--- app/shop-owner/notification.jsx
+++ app/shop-owner/notification.jsx
@@ -1,12 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import NotificationList from '../../components/shop-owner/Notification'
 
 const Notification = () => {
   return (
-    <View>
-      <Text>Notification</Text>
+    <View className="flex-1">
+      <Text className='text-center text-2xl my-4 font-semibold'>Notification</Text>
+      <NotificationList />
     </View>
-  )
+  );
 }
 
 export default Notification
