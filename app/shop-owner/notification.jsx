705b8ccefa60ed5a74b3f80aa91cbfa26fc4e541
Harry <phuothuynh2002@gmail.com>
2024-06-15 03:34:11 +0700

    Update Notification page

--- app/shop-owner/notification.jsx
+++ app/shop-owner/notification.jsx
@@ -1,66 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Text } from 'react-native';
-import NotificationList from '../../components/shop-owner/Notification';
-import { Button, Dialog, TextInput } from 'react-native-paper';
-import { Colors } from '../../constant';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import NotificationList from '../../components/shop-owner/Notification';
 
 const Notification = () => {
-  const [reason, setReason] = useState('');
-  const [deleteDialogVisible, setDeleteDialogVisible] = useState(false);
-  const showDeleteDialog = () => setDeleteDialogVisible(true);
-  const hideDeleteDialog = () => {
-    setDeleteDialogVisible(false);
-    setReason('');
-  };
-  const confirmDelete = () => {
-    hideDeleteDialog();
-  };
-
   return (
     <SafeAreaView className="flex-1">
-      <Text className="text-2xl font-bold m-4 text-center tracking-wider text-primary">
-        Thông báo đơn hàng mới
-      </Text>
-      <NotificationList showDeleteDialog={showDeleteDialog} />
-
-      {/* Dialog for cancel order */}
-      <Dialog className="bg-slate-100" visible={deleteDialogVisible} onDismiss={hideDeleteDialog}>
-        <Dialog.Title className="text-center">Lý do từ chối</Dialog.Title>
-        <Dialog.Content>
-          <TextInput
-            label="Vui lòng nhập lý do"
-            value={reason}
-            onChangeText={setReason}
-            mode="outlined"
-            outlineStyle={{ borderRadius: 12 }}
-            className="text-base"
-            activeOutlineColor="#DF4830"
-          />
-        </Dialog.Content>
-        <Dialog.Actions>
-          <Button
-            className="px-5"
-            mode="elevated"
-            textColor={Colors.primaryBackgroundColor}
-            onPress={hideDeleteDialog}
-          >
-            Hủy
-          </Button>
-          <Button
-            mode="contained"
-            className="px-5"
-            buttonColor={Colors.primaryBackgroundColor}
-            onPress={confirmDelete}
-            disabled={reason === ''}
-            labelStyle={{
-              fontSize: 14,
-            }}
-          >
-            Xác nhận
-          </Button>
-        </Dialog.Actions>
-      </Dialog>
+      <Text className="text-center text-2xl my-4 font-semibold">Thông báo</Text>
+      <NotificationList />
     </SafeAreaView>
   );
 };
